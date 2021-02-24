6af1cc9ea27ac522d2a93bfe9dc1ec1ec7ac419e
Gouri komti <gourikomtig@gmail.com>
2021-02-24 19:13:33 +0530

    CMS

--- src/components/What_we_do.js
+++ src/components/What_we_do.js
@@ -2,17 +2,36 @@ import { Card } from '@material-ui/core'
 import React from 'react'
 import '../css/What_we_do.css'
 import weird from '../Assets/Group 42.png'
+import {client} from './client.js';
 
-function What_we_do() {
-    return (
+
+
+
+class What_we_do extends React.Component{
+
+    state={
+        articles:[]
+    }
+    componentDidMount(){
+        client.getEntries()
+        .then((response) =>{
+            console.log(response)
+        })
+        .catch(console.error)
+    }
+
+    render () {
+        return(
+
+
         <div className="main" >
             <h2>What we do</h2>
             <img src={weird} alt="" style={{width:'28% ',marginLeft:'-4vw',marginTop:'-10vw'}}/>
+         </div>
 
-           
-     
-        </div>
-    )
+
+        );
+}
 }
 
 export default What_we_do
---
+++ src/components/client.js
@@ -0,0 +1,8 @@
+import * as contentful from 'contentful';
+
+
+export const client = contentful.createClient({
+    space: 'c27v8dm349y6',
+    accessToken: '-CA-i02gKACX7lHbyfGG6tW9V0jDXHJNTXCYaKYS3uw'
+})
+
